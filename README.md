191f9bdb5ca784ac1df0586b439977a930e7d479
Tim Dettrick <t.dettrick@uq.edu.au>
2013-03-27 17:27:53 +1000

    redis from e0ae116f5f4ec025c0f51a2df818c441b13a335d

---
+++ README.md
@@ -0,0 +1,81 @@
+# Redis Plugin
+
+This plugin provides support for [Redis](http://redis.io/) using the best Java driver [Jedis](https://github.com/xetorthio/jedis) and the corresponding Scala wrapper [Sedis](https://github.com/pk11/sedis). Also implements play's internal [Caching] (https://github.com/playframework/Play20/blob/master/framework/src/play/src/main/scala/play/api/cache/Cache.scala#L9) interface  
+
+# Features
+
+*  Provides a Redis-based Cache API (supported types: String, Int, Long, Boolean and Serializable) ie.
+
+```java
+//java
+String f = (String) play.cache.Cache.get("mykey");
+```
+
+and 
+
+```scala
+//scala
+val o = play.api.cache.Cache.getAs[String]("mykey")
+```
+
+* configurable ( variables: ```redis.host```, ```redis.port```, ```redis.timeout```, ```redis.password```, defaults are ```localhost```, ```6379```, ```2000```, ```nul``` )
+
+* Allows direct access to Jedis and Sedis: 
+
+```java
+//java
+import com.typesafe.plugin.RedisPlugin;
+import redis.clients.jedis.*;
+
+Jedis j = play.Play.application().plugin(RedisPlugin.class).jedisPool().getResource();
+
+try {
+  /// ... do stuff here 
+  j.set("foo", "bar");
+} finally {
+  play.Play.application().plugin(RedisPlugin.class).jedisPool().returnResource(j);
+}  
+```
+
+```scala
+//scala
+import play.api.Play.current
+import com.typesafe.plugin.RedisPlugin
+
+val pool = use[RedisPlugin].sedisPool
+pool.withJedisClient { client =>
+  Option[String] single = Dress.up(client).get("single")
+}
+```
+# How to install
+
+* add 
+
+play 2.0.x:
+```"com.typesafe" %% "play-plugins-redis" % "2.0.4"``` to your dependencies
+
+* create a file called ```play.plugins``` in your ```app/conf``` directory
+
+* add ```550:com.typesafe.plugin.RedisPlugin```
+
+*  while this plugin is going to be loaded before the default cache implementation,  it's a good practice to disable the overwritten plugin:
+
+```
+#conf/application.conf
+ehcacheplugin=disabled
+```
+
+# Sample
+
+for an example, see the bundled sample app
+
+
+## Licence
+
+This software is licensed under the Apache 2 license, quoted below.
+
+Copyright 2012 Typesafe (http://www.typesafe.com).
+
+Licensed under the Apache License, Version 2.0 (the "License"); you may not use this project except in compliance with the License. You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0.
+
+Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
---
+++ src/main/scala/com/typesafe/plugin/RedisPlugin.scala
@@ -0,0 +1,146 @@
+package com.typesafe.plugin
+
+import play.api._
+import org.sedis._
+import redis.clients.jedis._
+import play.api.cache._
+import java.util._
+import java.io._
+import biz.source_code.base64Coder._
+
+/**
+ * provides a redis client and a CachePlugin implementation
+ * the cache implementation can deal with the following data types:
+ * - classes implement Serializable
+ * - String, Int, Boolean and long
+ */
+class RedisPlugin(app: Application) extends CachePlugin {
+
+ 
+ private lazy val host = app.configuration.getString("redis.host").getOrElse("localhost")
+ private lazy val port = app.configuration.getInt("redis.port").getOrElse(6379)
+ private lazy val timeout = app.configuration.getInt("redis.timeout").getOrElse(2000)
+ private lazy val password = app.configuration.getString("redis.password").getOrElse(null)
+
+ /**
+  * provides access to the underlying jedis Pool
+  */
+ lazy val jedisPool = new JedisPool(new JedisPoolConfig(), host, port, timeout, password)
+
+  /**
+  * provides access to the sedis Pool
+  */           
+ lazy val sedisPool = new Pool(jedisPool)
+
+ override def onStart() {
+    sedisPool
+ }
+
+ override def onStop() {
+    jedisPool.destroy()
+ }
+
+ override lazy val enabled = {
+    !app.configuration.getString("redisplugin").filter(_ == "disabled").isDefined
+  }
+
+ /**
+  * cacheAPI implementation
+  * can serialize, deserialize to/from redis
+  * value needs be Serializable (a few primitive types are also supported: String, Int, Long, Boolean) 
+  */
+ lazy val api = new CacheAPI {
+
+    def set(key: String, value: Any, expiration: Int) {
+     var oos: ObjectOutputStream = null
+     var dos: DataOutputStream = null
+     try { 
+       val baos = new ByteArrayOutputStream()
+       var prefix = "oos"
+       if (value.getClass.isInstanceOf[Serializable]) {
+          oos = new ObjectOutputStream(baos)
+          oos.writeObject(value)
+          oos.flush()
+       } else if (value.getClass.isInstanceOf[String]) {
+          dos = new DataOutputStream(baos)
+          dos.writeUTF(value.asInstanceOf[String])
+          prefix = "string"
+       } else if (value.getClass.isInstanceOf[Int]) {
+          dos = new DataOutputStream(baos)
+          dos.writeInt(value.asInstanceOf[Int])
+          prefix = "int"
+       } else if (value.getClass.isInstanceOf[Long]) {
+          dos = new DataOutputStream(baos)
+          dos.writeLong(value.asInstanceOf[Long])
+          prefix = "long"
+       } else if (value.getClass.isInstanceOf[Boolean]) {
+          dos = new DataOutputStream(baos)
+          dos.writeBoolean(value.asInstanceOf[Boolean]) 
+          prefix = "boolean"   
+       } else {
+          throw new IOException("could not serialize: "+ value.toString)
+       }
+       val redisV = prefix + "-" + new String( Base64Coder.encode( baos.toByteArray() ) )  
+       //Logger.debug(redisV)
+       sedisPool.withJedisClient { client =>
+          client.set(key,redisV)
+          if (expiration != 0) client.expire(key,expiration)
+       }
+     } catch {case ex: IOException =>
+       Logger.warn("could not serialize key:"+ key + " and value:"+ value.toString + " ex:"+ex.toString)
+     } finally {
+       if (oos != null) oos.close()
+       if (dos != null) dos.close()
+     }
+    
+    }
+    def remove(key: String): Unit =  sedisPool.withJedisClient { client => client.del(key) }
+
+    def get(key: String): Option[Any] = {
+      var ois: ObjectInputStream = null
+      var dis: DataInputStream = null
+      try {
+        val data: Seq[String] =  sedisPool.withJedisClient { client =>
+            client.get(key)
+        }.split("-")
+        val b = Base64Coder.decode(data.last)
+        data.head match {
+          case "oos" => 
+              ois = new java.io.ObjectInputStream(new java.io.ByteArrayInputStream(b)) {
+                override protected def resolveClass(desc: ObjectStreamClass) = {
+                  Class.forName(desc.getName(), false, play.api.Play.current.classloader)
+                }
+              }
+              val r = ois.readObject()
+              Some(r)
+          case "string" => 
+              dis = new DataInputStream(new ByteArrayInputStream(b))
+              val r  = dis.readUTF()
+              Some(r)
+          case "int" =>  
+              dis = new DataInputStream(new ByteArrayInputStream(b))
+               val r  = dis.readInt
+              Some(r)
+          case "long" => 
+              dis = new DataInputStream(new ByteArrayInputStream(b))
+              val r  = dis.readLong
+              Some(r)
+          case "boolean" =>   
+              dis = new DataInputStream(new ByteArrayInputStream(b))
+              val r  = dis.readBoolean
+              Some(r)
+          case _ => throw new IOException("can not recognize value")
+        }
+        
+      } catch {case ex: Exception => 
+        Logger.debug("could not deserialize key:"+ key+ " ex:"+ex.toString)
+        //ex.printStackTrace()
+        None
+      } finally {
+       if (ois != null) ois.close()
+       if (dis != null) dis.close()
+      }
+    }
+
+  }
+}
